591497bd1864fee8395e2b75b8ea69049343216b
Anthony Peruso <perusoa@gmail.com>
2017-10-22 16:23:40 -0400

    Add video details component

---
+++ youtube-clone-react/src/components/video_detail.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const VideoDetail = (props) => {
+  const video = props.video;
+
+  //If there is no video display the words loading
+  if ( !video ) {
+    return <div>Loading...</div>;
+  }
+
+  //If there is no video description display a simple message
+  if ( video.snippet.description == '' ) {
+    video.snippet.description = 'Sorry, there is no description available for this video.';
+  }
+
+  //Get the ID of the video
+  const videoId = video.id.videoId;
+
+  //Build the URL for the iframe embed
+  const url = `https://www.youtube.com/embed/${videoId}`;
+
+  return (
+    <div className="video-detail">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" src={ url }></iframe>
+      </div>
+      <div className="details">
+        <span className="publisher">Published by: <span className="publisher-name">{ video.snippet.channelTitle }</span></span>
+        <h1 className="video-title">{ video.snippet.title }</h1>
+        <div className="video-description">{ video.snippet.description }</div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoDetail;
