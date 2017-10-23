abb32db7f177e0377f595bfef73719978d03a18b
Anthony Peruso <perusoa@gmail.com>
2017-10-23 08:36:55 -0400

    Make URBN the default search term

--- youtube-clone-react/src/index.js
+++ youtube-clone-react/src/index.js
@@ -24,7 +24,7 @@ class App extends Component {
     };
 
     //Set an initial search term so that it doesn't show up blank
-    this.videoSearch('lacrosse');
+    this.videoSearch('URBN');
   }
 
   videoSearch(term) {
